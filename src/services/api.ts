95cdb986ce6011dd023d9afd8ba1a109f1dfb11e
Giovanni Almeida <giovanni.as.br@gmail.com>
2023-12-10 03:01:32 -0300

    feat: improve fetcher util

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,18 @@
-const api = (...args: Parameters<typeof fetch>) =>
-  fetch(...args).then((res) => res.json())
+class FetchError extends Error {
+  constructor(message: string, public status: number) {
+    super(message)
+  }
+}
 
-export { api }
+const api = (...args: Parameters<typeof fetch>) =>
+  fetch(...args).then(async (res) => {
+    const body = await res.json()
+
+    if (!res.ok) {
+      throw new FetchError(res.statusText || body.message, res.status)
+    }
+
+    return body
+  })
+
+export { FetchError, api }
