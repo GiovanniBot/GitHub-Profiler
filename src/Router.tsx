18fc6ae6d829b75620612b92b7d875f398effb78
Giovanni Almeida <giovanni.as.br@gmail.com>
2023-12-10 01:19:01 -0300

    feat: add searchBar layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,17 @@
 import { Home } from '@/pages/Home'
 import { Profile } from '@/pages/Profile'
 import { Route, Routes } from 'react-router-dom'
+import { WithSearchBarLayout } from './layouts/withSearchBarLayout'
 import { Repository } from './pages/Repository'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/repository" element={<Repository />} />
+      <Route path="/" element={<WithSearchBarLayout />}>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/repository" element={<Repository />} />
+      </Route>
     </Routes>
   )
 }
