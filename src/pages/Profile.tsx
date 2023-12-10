038b472739ebf0b0e397bc17dce0ee60d4d7d3f7
Giovanni Almeida <giovanni.as.br@gmail.com>
2023-12-10 04:31:39 -0300

    feat: start /profile page (WIP)

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,15 @@
-import { useParams } from 'react-router-dom'
+import { api } from '@/services/api'
+import { useLocation, useParams } from 'react-router-dom'
+import useSWR from 'swr'
 
 export function Profile() {
   const { userName } = useParams<'userName'>()
+  const { state } = useLocation()
+  const { data } = useSWR(`https://api.github.com/users/${userName}`, api, {
+    fallbackData: state?.githubUser,
+  })
+
+  console.log(data)
 
   return (
     <div>
