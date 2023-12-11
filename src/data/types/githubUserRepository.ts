4589c19f8152fd98c49808e502a37ad5ce1a65aa
Giovanni Almeida <giovanni.as.br@gmail.com>
2023-12-11 01:28:52 -0300

    feat: add new prop to Repository data types

--- src/data/types/githubUserRepository.ts
+++ src/data/types/githubUserRepository.ts
@@ -1,6 +1,14 @@
+import { GithubUserProps } from './githubUser'
+
 export interface RepositoryMetadataProps {
   node_id: string
   name: string
   stargazers_count: number
   html_url: string
+  owner: GithubUserProps
+}
+
+export interface RepositoryProps extends RepositoryMetadataProps {
+  description?: string
+  language?: string
 }
