3cea1386cd30f2fceb2ef1d4009fcdef57451da3
Giovanni Almeida <giovanni.as.br@gmail.com>
2023-12-11 00:31:22 -0300

    feat: add types for gitHub user and repository metadata

---
+++ src/data/types/githubUser.ts
@@ -0,0 +1,9 @@
+export interface GithubUserProps {
+  name?: string
+  login?: string
+  avatar_url?: string
+  followers: number
+  following: number
+  html_url: string
+  bio?: string
+}
---
+++ src/data/types/githubUserRepository.ts
@@ -0,0 +1,6 @@
+export interface RepositoryMetadataProps {
+  node_id: string
+  name: string
+  stargazers_count: number
+  html_url: string
+}
