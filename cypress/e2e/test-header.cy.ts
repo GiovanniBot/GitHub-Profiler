9a6b30ad5c9b5a2a3bae5f214541e4984e40ad68
Giovanni Almeida <giovanni.as.br@gmail.com>
2023-12-11 20:38:04 -0300

    feat: add header test

---
+++ cypress/e2e/test-header.cy.ts
@@ -0,0 +1,49 @@
+describe('test header component', () => {
+  beforeEach(() => {
+    cy.visit('/')
+
+    cy.get('input[placeholder="search for a github user..."]')
+      .type('diego3g')
+      .parent('form')
+      .submit()
+  })
+
+  it('should be able to navigate to the profile page and return to home by clicking on the Logo', () => {
+    cy.get('svg:eq(1)')
+      .should('exist')
+      .click()
+
+    cy.url().should('eq', 'http://localhost:5173/')
+  })
+
+  it('should be able to navigate to another user profile by header search', () => {
+    cy.get('#githubUser')
+      .type('giovannibot')
+      .parent('form')
+      .submit()
+
+    cy.contains('Giovanni Almeida')
+      .should('exist')
+  })
+
+  it('should be able to change the theme to light and dark.', () => {
+    cy.visit('/')
+    
+    cy.get('button:first')
+      .should('exist')
+      .click()
+    
+    cy.get('div[role="menuitem"]:nth-child(1)')
+      .click()
+
+    cy.get('html[class="light"]')
+      .should('exist')
+
+      cy.get('div[role="menuitem"]:nth-child(2)')
+      .click()
+
+    cy.get('html[class="dark"]')
+      .should('exist')
+  })
+
+})
