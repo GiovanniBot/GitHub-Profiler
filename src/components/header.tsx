d2d3c2d3c11069902d7d8ad13817a53f422553f6
Giovanni Almeida <giovanni.as.br@gmail.com>
2023-12-10 21:29:54 -0300

    feat: finish header for profile and repository page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,61 @@
+import Logo from '@/assets/logo.svg?react'
+import { useSearchUserHook } from '@/hooks/useSearchUserHook'
+import { CornerDownLeft, Search } from 'lucide-react'
+import { useRef } from 'react'
+import { ThemeToggle } from './theme-toggle'
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+
 export function Header() {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const { isLoading, handleSubmit } = useSearchUserHook({
+    onSuccess: () => {
+      if (inputRef.current) {
+        inputRef.current.value = ''
+        inputRef.current.blur()
+      }
+    },
+  })
+
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="flex w-full p-5 h-14 items-center justify-between sticky top-0 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b mb-6">
+      <div>
+        <Logo className="dark:invert h-10 w-fit" />
+      </div>
+
+      <div className="flex gap-3">
+        <form
+          onSubmit={handleSubmit}
+          className="border rounded-md flex items-center px-3"
+        >
+          <label htmlFor="githubUser" className="cursor-pointer">
+            <Search className="text-gray-500 dark:text-gray-400" size={18} />
+          </label>
+          <Input
+            ref={inputRef}
+            disabled={isLoading}
+            className="focus-visible:shadow-none focus-visible:outline-none focus-visible:ring-0 border-none rounded-none h-5"
+            required
+            placeholder="Search for a user"
+            type="search"
+            id="githubUser"
+            name="githubUser"
+            pattern=".*\S+.*"
+            title="Please enter at least one non-whitespace character"
+          />
+          <Button
+            className="bg-transparent p-0 hover:bg-transparent"
+            type="submit"
+            disabled={isLoading}
+          >
+            <CornerDownLeft
+              className="text-gray-500 dark:text-gray-400"
+              size={18}
+            />
+          </Button>
+        </form>
+        <ThemeToggle />
+      </div>
     </div>
   )
 }
