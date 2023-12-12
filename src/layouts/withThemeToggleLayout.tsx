d09216106c45aed061ed65ba502d310db4dbd4e5
Giovanni Almeida <giovanni.as.br@gmail.com>
2023-12-12 18:15:59 -0300

    refactor: improved withThemeToggleLayout

--- src/layouts/withThemeToggleLayout.tsx
+++ src/layouts/withThemeToggleLayout.tsx
@@ -4,8 +4,12 @@ import { Outlet } from 'react-router-dom'
 export function WithThemeToggleLayout() {
   return (
     <>
-      <div className="fixed top-4 right-4">
-        <ThemeToggle />
+      <div className='w-full absolute z-10'>
+        <div className="flex items-center mx-auto mt-5 md:mt-3 w-11/12 z-10 relative">
+          <div className='flex items-center ms-auto gap-1 xs:gap-4'>
+            <ThemeToggle />
+          </div>
+        </div>
       </div>
       <Outlet />
     </>
